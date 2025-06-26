64d1ad315a8c74bbee82eb3ab4ceebd417d3529a
reigetsuotowa <reigetsuotowa@Otowa-Macmini.local>
2025-06-26 21:00:11 +0800

    Fixed some bugs

--- src/components/Setting/MainSetting.vue
+++ src/components/Setting/MainSetting.vue
@@ -18,7 +18,7 @@
           <SvgIcon name="Github" :size="20" />
           {{ packageJson.author }}
         </n-text>
-        <n-text class="name">SPlayer</n-text>
+        <n-text class="name">HaruMelody</n-text>
         <n-text class="version" depth="3">{{ packageJson.version }}</n-text>
       </div>
     </div>
